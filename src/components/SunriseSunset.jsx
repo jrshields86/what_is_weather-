24b14bd6e2e3c4bf77f5b30b5de51dbaf20b664b
jrshields86 <jrshields86@gmail.com>
2024-04-09 22:00:26 -0600

    sunrise & sunset partially displaying and displaying correctly

--- src/components/SunriseSunset.jsx
+++ src/components/SunriseSunset.jsx
@@ -1,27 +1,38 @@
+import './sunriseSunset.css'
+
 const SunriseSunset = ({data}) => {
     console.log(data);
     const sunriseData = data.sys.sunrise;
     const sunsetData = data.sys.sunset;
-
     const sunriseTime = new Date(sunriseData * 1000);
-    console.log('Sunrise -->', sunriseTime);
-
     const sunsetTime = new Date(sunsetData * 1000);
-    console.log('Sunset -->', sunsetTime);
-
     const sunriseHours = sunriseTime.getHours();
-    console.log(sunriseHours);
-
     const sunriseMinutes = sunriseTime.getMinutes();
-    console.log(sunriseMinutes)
-
     const sunsetHours = sunsetTime.getHours();
     const sunsetMinutes = sunsetTime.getMinutes();
-
-    console.log(`${sunriseHours}:${sunriseMinutes} a.m.`);
-    console.log(`${sunsetHours - 12}:${sunsetMinutes} p.m.`);
+    const formattedSunrise = `${sunriseHours}:${sunriseMinutes} a.m.`;
+    const formattedSunset = `${sunsetHours - 12}:${sunsetMinutes} p.m.`;
+    console.log(formattedSunrise, formattedSunset);
+    
     return (
-        <h2>Sunrise Sunset</h2>
+        <>
+            <h2>Sunrise & Sunset</h2>
+            
+            <div className="sunriseSunsetContainer">
+                <div id='sunriseText'>
+                    Sunrise
+                </div>
+                <div id='sunsetText'>
+                    Sunset
+                </div>
+                <div id="sunrise">
+                    {formattedSunrise}
+                </div>
+                <div id="sunset">
+                    {formattedSunset}
+                </div>
+            </div>
+        </>
     );
 };
 
